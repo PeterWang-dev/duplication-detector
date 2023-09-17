f4d0d7ad7e7e9c9b8125de6315d0bfd9220e240a
Peter Wang <59994316+PeterWang-dev@users.noreply.github.com>
2023-09-17 17:54:54 +0800

    [Release] duplicate-detection v0.1.0 (#16)
    * docs: update README.md
    
    * docs: add blog
    
    * chore: update vscode extension recommendations
    
    * docs: supply figure for blog
    
    * feat(dd-cli): architect `dd-cli` with basic codes
    
    * feat(dd-lib): architect `dd-lib`
    
    * refactor: intergrate dd-lib with dd-cli
    
    * chore: update extension recommendations
    
    * Feature/preprocessor (#5)
    
    * chore: update extension recommendations
    
    * feat(lib): implement preprocessor preliminarily
    
    Implemented #4
    
    * Feature/cli parser (#6)
    
    * style(bin): rename cli to config test
    
    Related to #2
    
    * Feature/processor (#7)
    
    * feat(lib): implement processor preliminarily
    
    Implemented #3
    
    * Refactor/structure_tests (#11)
    
    * feat(tests): implement integration test
    
    Resolves #10.
    
    * refactor(tests): split complex tests to easy ones
    
    Resolves #10
    
    * fix(lib): fix related errors of ratio calculation (#12)
    
    * fix(lib): fix related errors of ratio calculation
    
    1. Fixed wrong duplication rate;
    2. Fixed wired function in unittest;
    3. Fixed error in integration test.
    
    Fixes #9
    
    * fix(lib): remove unnecessary std::result
    
    * Chore/codelldb-extension (#13)
    
    * chore: add extension recommendation of codelldb
    
    * chore: place codelldb ext in the front
    
    * fix(tests): supply the inefficient tests (#14)

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+use std::path::PathBuf;
+
+pub mod preprocessor;
+pub mod processor;
+
+#[derive(Debug)]
+pub struct Config {
+    original_path: PathBuf,
+    input_path: PathBuf,
+    output_path: PathBuf,
+}
+
+impl Config {
+    pub fn from(original_path: &str, input_path: &str, output_path: &str) -> Self {
+        Config {
+            original_path: PathBuf::from(original_path),
+            input_path: PathBuf::from(input_path),
+            output_path: PathBuf::from(output_path),
+        }
+    }
+
+    pub fn original_path(&self) -> &PathBuf {
+        &self.original_path
+    }
+
+    pub fn input_path(&self) -> &PathBuf {
+        &self.input_path
+    }
+
+    pub fn output_path(&self) -> &PathBuf {
+        &self.output_path
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_config_from() {
+        let config = Config::from("./original.txt", "./input.txt", "./output.txt");
+
+        assert_eq!(config.original_path, PathBuf::from("./original.txt"));
+        assert_eq!(config.input_path, PathBuf::from("./input.txt"));
+        assert_eq!(config.output_path, PathBuf::from("./output.txt"));
+    }
+
+    #[test]
+    fn test_config_getter() {
+        let config = Config::from("./original.txt", "./input.txt", "./output.txt");
+
+        assert_eq!(config.original_path(), &PathBuf::from("./original.txt"));
+        assert_eq!(config.input_path(), &PathBuf::from("./input.txt"));
+        assert_eq!(config.output_path(), &PathBuf::from("./output.txt"));
+    }
+}

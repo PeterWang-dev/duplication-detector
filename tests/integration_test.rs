8da3d710073552fc4abdea4c2252ac3efec4df4a
Peter Wang <59994316+PeterWang-dev@users.noreply.github.com>
2023-09-17 17:30:13 +0800

    fix(tests): supply the inefficient tests (#14)

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -27,7 +27,38 @@ fn test_full_functionalities() {
     assert!(result_string.parse::<f64>().unwrap() > 0.50);
 
     // Clean up
-    fs::remove_file("./original.txt").unwrap();
-    fs::remove_file("./input.txt").unwrap();
-    fs::remove_file("./output.txt").unwrap();
+    fs::remove_file(config.original_path()).unwrap();
+    fs::remove_file(config.input_path()).unwrap();
+    fs::remove_file(config.output_path()).unwrap();
+}
+
+#[test]
+fn test_with_example_inputs() {
+    // Use the example inputs from example/inputs.
+    let config = Config::from(
+        "examples/inputs/orig.txt",
+        "examples/inputs/orig_0.8_add.txt",
+        "examples/output.txt",
+    );
+
+    let original_string = preprocessor::trim_and_convert(config.original_path()).unwrap();
+    let input_string = preprocessor::trim_and_convert(config.input_path()).unwrap();
+
+    let mut dector = processor::Detector::new(original_string, input_string);
+    let ratio: f64 = dector.compute_ratio();
+
+    if fs::read(config.output_path()).is_ok() {
+        fs::remove_file(config.output_path()).unwrap();
+    }
+
+    let mut output_file = fs::File::create(config.output_path()).unwrap();
+    io::Write::write_fmt(&mut output_file, format_args!("{ratio:.2}")).unwrap();
+
+    let result_string = fs::read_to_string(config.output_path()).unwrap();
+    let result = result_string.parse::<f64>().unwrap();
+
+    assert!( result > 0.80 && result < 0.90);
+
+    // Clean up
+    fs::remove_file(config.output_path()).unwrap();
 }
